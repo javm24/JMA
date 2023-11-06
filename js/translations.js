402f1fbc4a9352aa895df48caa59cecb57d280a8
José Vargas Mena <jvargas@jmalogistica.com>
2023-11-06 10:39:29 -0600

    first commit

---
+++ js/translations.js
@@ -0,0 +1,247 @@
+var translations = {
+    "loading": {
+        "es": "Cargando...",
+        "en": "Loading...",
+        "pt": "Carregamento..."
+    },
+    "home": {
+        "es": "Inicio",
+        "en": "Home",
+        "pt": "Início"
+    },
+    "aboutus": {
+        "es": "Sobre nosotros",
+        "en": "About us",
+        "pt": "Sobre nós"
+    },
+    "service": {
+        "es": "Servicios",
+        "en": "Services",
+        "pt": "Serviços"
+    },
+    "contactus": {
+        "es": "Contáctenos",
+        "en": "Contact us",
+        "pt": "Entre em contato conosco"
+    },
+    "track": {
+        "es": "Rastrea tu pedido",
+        "en": "Track your order",
+        "pt": "Rastreie seu pedido"
+    },
+    "centerinthe": {
+        "es": "Centrarnos en el cliente",
+        "en": "Focus on the customer",
+        "pt": "Centrar a atenção no cliente"
+    },
+    "learn": {
+        "es": "Aprender rápido y adaptarse al cambio",
+        "en": "Learn fast and adapt to change",
+        "pt": "Aprender rapidamente e adaptar-se às mudanças"
+    },
+    "work": {
+        "es": "Trabajar en equipo para ganar",
+        "en": "Working as a team to win",
+        "pt": "Trabalhar em equipe para vencer"
+    },
+    "impulse": {
+        "es": "Impulsar resultados con rapidez y responsabilidad",
+        "en": "Boost results with speed and responsibility",
+        "pt": "Geração de resultados de forma rápida e responsável"
+    },
+    "operational": {
+        "es": "Excelencia operacional",
+        "en": "Operational excellence",
+        "pt": "Excelência operacional"
+    },
+    "who": {
+        "es": "¿Quiénes somos?",
+        "en": "Who we are?",
+        "pt": "Quem somos nós?"
+    },
+    "15years": {
+        "es": "Con mas de 15 años en el Mercado costarricense, nuestra compañia ha servido a nuestros clientes como un socio comercial experimentado, entregando calidad y personalización. Hemos desarrollado nuestros negocios y aumentado nuestros clientes. Nuestro negocio principal consta de una gran flota de camiones con diferentes capacidades de carga.",
+        "en": "With more than 15 years in the Costa Rican market, our company has served our customers as an experienced business partner, delivering quality and customization. We have developed our business and increased our customers. Our core business consists of a large fleet of trucks with different load capacities.",
+        "pt": "Com mais de 15 anos no mercado da Costa Rica, a nossa empresa tem servido os nossos clientes como um parceiro de negócios experiente, oferecendo qualidade e personalização. Desenvolvemos o nosso negócio e aumentámos os nossos clientes. A nossa atividade principal consiste numa grande frota de camiões com diferentes capacidades de carga."
+    },
+    "mission": {
+        "es": "Misión",
+        "en": "Mission",
+        "pt": "Missão"
+    },
+    "wework": {
+        "es": "En Grupo Logistico JMA, trabajamos arduamente para dar como su proveedor de transporte y logística: seguridad en su carga, puntualidad, traslado personalizado de su producto, manejo y entrega de su producto y orientarnos al servicio de sus clientes.",
+        "en": "In Grupo Logistico JMA, we work hard to provide as your transportation and logistics provider: safety in your cargo, punctuality, personalized transfer of your product, handling and delivery of your product and to be customer service oriented.",
+        "pt": "No Grupo Logistico JMA, trabalhamos arduamente para oferecer como seu fornecedor de transporte e logística: segurança na sua carga, pontualidade, transporte personalizado do seu produto, manuseamento e entrega do seu produto e para nos orientarmos para o serviço dos seus clientes."
+    },
+    "vision": {
+        "es": "Visión",
+        "en": "Vision",
+        "pt": "Visão"
+    },
+    "bea": {
+        "es": "Ser una compañía líder en el transporte de carga y logística de nuestros clientes por medio de eficientes maneras de transportar mercadería.",
+        "en": "Be a leading company in the transportation of cargo and logistics of our customers through efficient ways of transporting goods.",
+        "pt": "Ser uma empresa líder no transporte de carga e logística para os nossos clientes através de formas eficientes de transporte de mercadorias."
+    },
+    "data": {
+        "es": "Algunos datos sobre nosotros",
+        "en": "Some facts about us",
+        "pt": "Alguns fatos sobre nós"
+    },
+    "weare": {
+        "es": "Somos una empresa de logística especializada en la consolidación y movimientos de carga. Nuestro objetivo es brindar soluciones integrales y eficientes para el transporte y almacenamiento de mercancías.",
+        "en": "We are a logistics company specialized in the consolidation and movement of cargo. Our goal is to provide integral and efficient solutions for the transportation and storage of merchandise.",
+        "pt": "Somos uma empresa de logística especializada na consolidação e movimentação de cargas. O nosso objetivo é fornecer soluções completas e eficientes para o transporte e armazenamento de mercadorias."
+    },
+    "callus": {
+        "es": "Llámanos para cualquier consulta",
+        "en": "Call us for any question",
+        "pt": "Entre em contato conosco para esclarecer qualquer dúvida"
+    },
+    "clients": {
+        "es": "Clientes",
+        "en": "Clients",
+        "pt": "Clientes"
+    },
+    "vehicles": {
+        "es": "Móviles",
+        "en": "Vehicles",
+        "pt": "Veículos"
+    },
+    "kilometers": {
+        "es": "Kilómetros",
+        "en": "Kilometers",
+        "pt": "Quilómetros"
+    },
+    "ourservices": {
+        "es": "Nuestros servicios",
+        "en": "Our services",
+        "pt": "Os nossos serviços"
+    },
+    "landfreight": {
+        "es": "Transporte terrestre",
+        "en": "Land freight",
+        "pt": "Transporte terrestre"
+    },
+    "bulkcargo": {
+        "es": "Carga seca",
+        "en": "Bulk cargo",
+        "pt": "Carga a granel"
+    },
+    "refrigeratedload": {
+        "es": "Carga refrigerada",
+        "en": "Refrigerated load",
+        "pt": "Carga refrigerada"
+    },
+    "motorcycle": {
+        "es": "Servicio de motocicletas",
+        "en": "Motorcycle service",
+        "pt": "Serviço de motocicleta"
+    },
+    "courierservice": {
+        "es": "Mensajería",
+        "en": "Courier service",
+        "pt": "Serviço de correio"
+    },
+    "fleet": {
+        "es": "Mantenimiento de flotas vehículares",
+        "en": "Vehicle fleet maintenance",
+        "pt": "Manutenção da frota de veículos"
+    },
+    "trustable": {
+        "es": "Somos una empresa de logística de confianza",
+        "en": "We are a reliable logistics company",
+        "pt": "Somos uma empresa de logística fiável"
+    },
+    "allcountry": {
+        "es": "Servicio a todo el país",
+        "en": "Service to all the country",
+        "pt": "Serviço a todo o país"
+    },
+    "costarica": {
+        "es": "Nos destacamos por brindar nuestro servicio de logística a todos los rincones de Costa Rica.",
+        "en": "We stand out for offering our logistics service to every corner of Costa Rica.",
+        "pt": "Somos excelentes em fornecer nosso serviço de logística para todos os cantos da Costa Rica."
+    },
+    "ontime": {
+        "es": "Entregas a tiempo",
+        "en": "On-time deliveries",
+        "pt": "Entregas no prazo"
+    },
+    "ourclients": {
+        "es": "Trabajamos en estrecha colaboración con nuestros clientes para garantizar la entrega puntual de sus productos.",
+        "en": "We work closely with our customers to ensure on-time delivery of their products.",
+        "pt": "Trabalhamos em estreita colaboração com os nossos clientes para garantir a entrega pontual dos seus produtos."
+    },
+    "safe": {
+        "es": "Distribución segura",
+        "en": "Safe distribution",
+        "pt": "Distribuição segura"
+    },
+    "preserve": {
+        "es": "Tomamos todas las medidas de seguridad necesaria para preservar el buen estado de los productos transportados.",
+        "en": "We take all the necessary safety measures to preserve the good condition of the transported products.",
+        "pt": "Tomamos todas as medidas de segurança necessárias para preservar o bom estado dos produtos transportados."
+    },
+    "leaders": {
+        "es": "Líderes administrativos",
+        "en": "Administrative leaders",
+        "pt": "Dirigentes administrativos"
+    },
+    "partner": {
+        "es": "Socio",
+        "en": "Partner",
+        "pt": "Sócio"
+    },
+    "gm": {
+        "es": "Gerente General",
+        "en": "General Manager",
+        "pt": "Gerente Geral"
+    },
+    "address": {
+        "es": "Dirección",
+        "en": "Address",
+        "pt": "Endereço"
+    },
+    "services": {
+        "es": "Servicios",
+        "en": "Services",
+        "pt": "Serviços"
+    },
+    "quicklinks": {
+        "es": "Enlaces rápidos",
+        "en": "Quick links",
+        "pt": "Ligações rápidas"
+    },
+    "socialnetworks": {
+        "es": "Redes sociales",
+        "en": "Social networks",
+        "pt": "Redes sociais"
+    },
+    "contactusfor": {
+        "es": "Contáctenos para cualquier consulta",
+        "en": "Contact us for any inquiry",
+        "pt": "Entre em contato conosco para qualquer dúvida"
+    },
+    "name": {
+        "es": "Nombre o empresa",
+        "en": "Name or company",
+        "pt": "Nome ou empresa"
+    },
+    "subject": {
+        "es": "Asunto",
+        "en": "Subject",
+        "pt": "Assunto"
+    },
+    "message": {
+        "es": "Mensaje",
+        "en": "Message",
+        "pt": "Mensagem"
+    },
+    "btnEnviar": {
+        "es": "Enviar",
+        "en": "Send",
+        "pt": "Enviar"
+    },
+};
